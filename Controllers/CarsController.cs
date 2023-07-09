66b63c0996fb72e832c24ff5e98459337e8e0cde
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2023-07-09 22:43:16 +0300

    add cars controller

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMarketplace.Controllers
+{
+    public class CarsController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddMake(string name, IFormFile logo)
+        {
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
